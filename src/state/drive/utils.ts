3e9ccedb40db35f4a77a2a577a24e802245ab097
kadami <kadamidev@users.noreply.github.com>
2023-03-26 01:53:03 +0700

    new tree conversion function added

--- src/state/drive/utils.ts
+++ src/state/drive/utils.ts
@@ -1,26 +1,92 @@
 import {
   ResearchObjectComponentType,
   ResearchObjectV1,
-} from "@src/../../nodes/desci-models/dist";
+  ResearchObjectV1Component,
+} from "@desci-labs/desci-models";
+import { DRIVE_NODE_ROOT_PATH } from "@src/components/driveUtils";
+import {
+  AccessStatus,
+  DriveMetadata,
+  DriveNonComponentTypes,
+  DriveObject,
+  FileType,
+} from "@src/components/organisms/Drive";
+
+import { v4 as uuidv4 } from "uuid";
 
 export type cidString = string;
+export type componentId = string;
 
-export function generateCidTypeMap(
+export function generateCidCompMap(
   manifest: ResearchObjectV1
-): Record<cidString, ResearchObjectComponentType> {
-  const componentsMap: Record<cidString, ResearchObjectComponentType> = {};
+): Record<cidString, ResearchObjectV1Component> {
+  const componentsMap: Record<cidString, ResearchObjectV1Component> = {};
   manifest.components.forEach((c) => {
     switch (c.type) {
       case ResearchObjectComponentType.CODE:
       case ResearchObjectComponentType.PDF:
-        componentsMap[c.payload.url] = c.type;
+        componentsMap[c.payload.url] = c;
         return;
       case ResearchObjectComponentType.DATA:
-        componentsMap[c.payload.cid] = c.type;
+        componentsMap[c.payload.cid] = c;
         return;
       default:
         return;
     }
   });
   return componentsMap;
+}
+
+export function extractComponentMetadata(
+  component: ResearchObjectV1Component
+): DriveMetadata {
+  if (!component) return {};
+  const metadata: DriveMetadata = {};
+  const validMetadataKeys: (keyof DriveMetadata)[] = [
+    "title",
+    "keywords",
+    "description",
+    "licenseType",
+    "ontologyPurl",
+    "controlledVocabTerms",
+  ];
+
+  validMetadataKeys.forEach((k) => {
+    if (k in component.payload) metadata[k] = component.payload[k];
+  });
+
+  return metadata;
+}
+
+export const DRIVE_EXTERNAL_LINKS_PATH = "externallinks";
+
+//Convert IPFS tree to DriveObject tree V2
+export function convertIpfsTreeToDriveObjectTree(
+  tree: DriveObject[],
+  cidToCompMap: Record<cidString, ResearchObjectV1Component>
+) {
+  tree.forEach((branch) => {
+    const component = cidToCompMap[branch.cid];
+    branch.componentType = component.type || DriveNonComponentTypes.UNKNOWN;
+    branch.accessStatus = AccessStatus.PRIVATE; // FIXME, HARDCODED, PRIVCIDMAP
+    branch.metadata = extractComponentMetadata(component);
+    branch.starred = component.starred || false;
+    branch.uid = component.id || uuidv4(); //add cached uuids
+    const pathSplit = branch.path?.split("/");
+    if (pathSplit) {
+      pathSplit[0] = DRIVE_NODE_ROOT_PATH;
+      branch.path = pathSplit.join("/");
+    }
+    if (
+      branch.contains &&
+      branch.contains.length &&
+      branch.type === FileType.Dir
+    ) {
+      branch.contains = convertIpfsTreeToDriveObjectTree(
+        branch.contains,
+        cidToCompMap
+      );
+    }
+  });
+  return tree;
 }
