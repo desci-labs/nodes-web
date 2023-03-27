7fcf3a09b7be96880fe91709117885c8c0f2ef86
shadrach-tayo <shadrachtemitayo@gmail.com>
2023-03-27 15:05:36 +0100

    migrate renameDataModel

--- src/components/organisms/Drive/RenameDataModal.tsx
+++ src/components/organisms/Drive/RenameDataModal.tsx
@@ -3,10 +3,11 @@ import {
   ResearchObjectV1,
 } from "@desci-labs/desci-models";
 import DefaultSpinner from "@src/components/atoms/DefaultSpinner";
-import PopOverBasic from "@src/components/atoms/PopOverBasic";
 import PrimaryButton from "@src/components/atoms/PrimaryButton";
 import PopoverFooter from "@src/components/molecules/Footer";
 import InsetLabelInput from "@src/components/molecules/FormInputs/InsetLabelInput";
+import Modal from "@src/components/molecules/Modal/Modal";
+import { IconX } from "@src/icons";
 import { useNodeReader } from "@src/state/nodes/hooks";
 import { updateComponent, saveManifestDraft } from "@src/state/nodes/viewer";
 import { useSetter } from "@src/store/accessors";
@@ -105,30 +106,39 @@ const RenameDataModal: React.FC<RenameDataModalProps> = ({
   const componentString = getComponentString(compType);
 
   return (
-    <div>
-      <PopOverBasic
-        bodyClassNames="!min-h-[70px]"
-        onClose={() => {
-          setRenameComponentId(null);
-        }}
-        title={`Rename ${componentString}`}
-        isVisible={true}
-        footer={() => (
-          <PopoverFooter>
-            <PrimaryButton onClick={handleSubmit}>
-              {loading ? <DefaultSpinner color="black" size={24} /> : "Save"}
-            </PrimaryButton>
-          </PopoverFooter>
-        )}
-      >
-        <InsetLabelInput
-          label={`${componentString} Name`}
-          value={newName}
-          onChange={(e: any) => setNewName(e.target.value)}
-          mandatory={true}
-        />
-      </PopOverBasic>
-    </div>
+    <Modal
+      onDismiss={() => {
+        setRenameComponentId(null);
+      }}
+      isOpen
+    >
+      <div className="py-3 px-6 !min-h-[70px] min-w-[400px]">
+        <div className="flex flex-row justify-between items-center">
+          <h1 className="text-lg font-bold text-white">{`Rename ${componentString}`}</h1>
+          <div
+            className="cursor-pointer p-5 -m-5 absolute right-5 top-5 stroke-black dark:stroke-white hover:stroke-muted-300 hover:dark:stroke-muted-300"
+            onClick={() => {
+              setRenameComponentId(null);
+            }}
+          >
+            <IconX />
+          </div>
+        </div>
+        <div className="my-2">
+          <InsetLabelInput
+            label={`${componentString} Name`}
+            value={newName}
+            onChange={(e: any) => setNewName(e.target.value)}
+            mandatory={true}
+          />
+        </div>
+      </div>
+      <PopoverFooter>
+        <PrimaryButton onClick={handleSubmit}>
+          {loading ? <DefaultSpinner color="black" size={24} /> : "Save"}
+        </PrimaryButton>
+      </PopoverFooter>
+    </Modal>
   );
 };
 
