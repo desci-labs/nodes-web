7945bc7d105083dd495ede0d342b698ffdd81063
shadrach-tayo <shadrachtemitayo@gmail.com>
2023-04-08 15:26:13 +0100

    cleanup

--- src/App/PrivateViewer.tsx
+++ src/App/PrivateViewer.tsx
@@ -1,30 +1,16 @@
 import ManuscriptReader from "@src/components/organisms/ManuscriptReader";
 import PdfHeader from "@src/components/organisms/PdfHeader";
 import useScroll from "@src/hooks/useScroll";
-import { useLocation } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
 import { Popovers } from "./Providers/AppWrapper";
-import { useUser } from "@src/state/user/hooks";
+// import { useUser } from "@src/state/user/hooks";
 
-// const auth = localStorage.getItem("auth");
 export default function PrivateViewer() {
   // page scroll behaviour init hook
   useScroll();
-  const user = useUser();
-  const location = useLocation();
-  // const navigate = useNavigate();
-  console.log("Private Viewer", user, location);
-
-  // useEffect(() => {
-  //   if (auth) {
-  //     if (user) {
-  //       if (location.pathname === "/" || location.pathname === "web/magic") {
-  //         navigate(`${site.app}${app.nodes}`);
-  //       }
-  //     }
-  //   } else if (location.pathname === "/") {
-  //     navigate(`${site.web}${location.search}`);
-  //   }
-  // }, [user, location.pathname, navigate, location.search]);
+  // const user = useUser();
+  // const location = useLocation();
+  // console.log("Private Viewer", user, location);
 
   return (
     <div
