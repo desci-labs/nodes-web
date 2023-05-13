3a5134f350f3bc096cd413e07888b25133dc3364
shadrach-tayo <shadrachtemitayo@gmail.com>
2023-05-13 03:08:51 +0100

    update add annotation button style

--- src/components/molecules/FloatingActionBar.tsx
+++ src/components/molecules/FloatingActionBar.tsx
@@ -2,7 +2,7 @@ import TooltipIcon from "@components/atoms/TooltipIcon";
 import { useManuscriptController } from "@src/components/organisms/ManuscriptReader/ManuscriptController";
 import { lockScroll } from "@components/utils";
 import { ResearchObjectComponentType } from "@desci-labs/desci-models";
-import { IconAdd, IconAddComment, IconRemoveComment } from "@icons";
+import { IconAdd, IconAnnotate, IconRemoveComment } from "@icons";
 import * as React from "react";
 import { useNodeReader, usePdfReader } from "@src/state/nodes/hooks";
 import { useSetter } from "@src/store/accessors";
@@ -17,11 +17,11 @@ const FloatingActionBar = () => {
 
   const AnnotationButton = () => {
     return (
-      <IconAddComment
-        height={24}
-        width={24}
-        fill="white"
-        className={`group-hover:fill-[#28AAC4] transition ease duration-100`}
+      <IconAnnotate
+        height={28}
+        width={28}
+        fill="none"
+        className={``}
       />
     );
   };
@@ -86,7 +86,7 @@ const FloatingActionBar = () => {
     />,
     <div
       key="tooltip-component-annotate"
-      className="w-8 h-8 rounded-full flex items-center justify-center group transition-transform bg-black cursor-pointer"
+      className="w-10 h-10 rounded-full flex items-center justify-center group transition-transform bg-transparent hover:bg-neutrals-gray-2 cursor-pointer"
       onClick={() => {
         dispatch(setIsAnnotating(!isAnnotating));
         dispatch(setAnnotatingViaButton(true));
@@ -104,7 +104,7 @@ const FloatingActionBar = () => {
   if (mode === "editor" && !isEditingAnnotation && !isCode) {
     return (
       <div className="fixed top-[50px] left-[-8px] z-0">
-        <div className="rounded-full w-12 h-[105px] text-white fill-current bg-black m-8 flex items-center pt-2 drop-shadow-lg shadow-lg flex-col gap-5">
+        <div className="rounded-full w-[50px] h-fit py-2 text-white fill-current bg-black m-8 flex items-center justify-center flex-col gap-3 drop-shadow-lg shadow-lg">
           {children}
         </div>
       </div>
